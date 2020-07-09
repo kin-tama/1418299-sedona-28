d319cb2551cb6537b79df9b83795b7f6bd7490d0
kin-tama <65250935+kin-tama@users.noreply.github.com>
2020-07-09 23:34:12 +0300

    поправил JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 var formButton = document.querySelector(".find-hotel-button");
 var findForm = document.querySelector(".find-hotel-form");
+var kidsNumber = findForm.querySelector(".kids");
+var adultsNumber = findForm.querySelector(".adults");
 
 formButton.addEventListener("click", function (evt) {
-findForm.classList.toggle("visually-hidden");
+findForm.classList.toggle("hide");
 });
+
+findForm.addEventListener("submit", function (evt) {
+localStorage.setItem("kidsNumber", kidsNumber.value);
+localStorage.setItem("adultsNumber", adultsNumber.value);});
